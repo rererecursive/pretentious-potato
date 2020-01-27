7de4cb812a593ca625ff58d078fdffdcd1499521
zac <z.t.lewis@lavabit.com>
2020-01-27 23:36:19 +1100

    Support for Rust crates.

--- vars/getLatestPackages.groovy
+++ vars/getLatestPackages.groovy
@@ -13,7 +13,7 @@ def call(config) {
     def installPath = config.get('installPath', '/tmp')
     def ignore = config.get('ignore', []).join('|')
 
-    docker.image('rererecursive/latest-packages').inside("-v ${installPath}:/host") {
+    docker.image('rererecursive/latest-packages').inside("-v ${installPath}:/host -v $PWD:/app") {
         sh "python3 main.py file=${config.file} type=${config.type} ignore=${ignore}"
 
         if (config.installPath) {
